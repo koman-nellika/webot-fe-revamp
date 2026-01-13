5e2f3ca23405434b1276a6524fbba603c7d655e6
Komen Bauaonsuppasri_nellika <Komen.b@nellika.co.th>
2026-01-13 10:23:33 +0700

    fix : matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -23,5 +23,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)", "/"],
+  matcher: ["/((?!api|_next/static|_next/image|auth|images|favicon.ico).*)", "/"],
 };
